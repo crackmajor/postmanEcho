f2e6a64feabbfe59aafbf5f05e854b3f3a58e2a8
Ivan Korekin <c_r_a_c_k@mail.ru>
2022-07-27 17:06:01 +0300

    first commit, test failed

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class PostmanEchoTest{
+    @Test
+    void postmanEchoTest(){
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data agfagfadgdgd") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data "))
+        ;
+    }
+}
